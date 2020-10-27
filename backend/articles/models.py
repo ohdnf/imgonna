a67b23eb1703f116b33cbc081be748533a46a739
Jupyo Hong <jupyohong7@gmail.com>
2020-10-28 01:52:29 +0900

    Update node-forge dependency

--- backend/articles/models.py
+++ backend/articles/models.py
@@ -17,4 +17,3 @@ class ArticleComment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
